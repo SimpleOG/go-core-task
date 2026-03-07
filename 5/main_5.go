8bcca594ea39759e25c6ca59c34cbbd9b2ef1074
Oleg Gagloev <ogagloev@team.amocrm.com>
2026-03-07 21:58:06 +0300

    added code and tests for intersections func from task 5

--- 5/main_5.go
+++ 5/main_5.go
@@ -1,3 +1,31 @@
 package main
 
-func main() {}
+import "fmt"
+
+func FindIntersections(arr1, arr2 []int) (bool, []int) {
+	firstValues := make(map[int]struct{}, len(arr1))
+	result := make([]int, 0, len(arr1)/2)
+	var haveIntersections bool
+	for _, v := range arr1 {
+		if _, ok := firstValues[v]; !ok {
+			firstValues[v] = struct{}{}
+		}
+	}
+	for _, v := range arr2 {
+		if _, ok := firstValues[v]; ok {
+			result = append(result, v)
+			//Чтобы избежать записи одинаковых пересечений несколько раз
+			delete(firstValues, v)
+		}
+	}
+	if len(result) > 0 {
+		haveIntersections = true
+	}
+	return haveIntersections, result
+}
+
+func main() {
+	a := []int{65, 3, 58, 678, 64}
+	b := []int{64, 2, 3, 43}
+	fmt.Println(FindIntersections(a, b))
+}
